6bd93e14aeb084412f5642d2c8e4c840db43b016
Jorobraun <jorobraun2007@gmail.com>
2026-04-16 17:57:56 +0200

    Pygame idea

--- src/main.sv
+++ src/main.sv
@@ -3,6 +3,7 @@
 `include "hvsync_generator.sv"
 
 module main (
+  // Diese Sachen werden wie Python File gesteuert.
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,6 +17,8 @@ module main (
   wire [9:0] hpos;
   wire [9:0] vpos;
 
+
+  // Hilfszeug für Display berechnung.
   hvsync_generator hvsync_generator (
     clk,
     ~rst_n,
@@ -28,6 +31,7 @@ module main (
 
   assign uo_out = {hsync, 1'b0, 1'b1, 1'b0, vsync, 1'b0, 1'b1, 1'b0};
 
+  // Wir dumben eine wave.vcd datei raus um zu verstehen was abgeht.
   initial begin
     $dumpfile("wave.vcd");
     $dumpvars(0, main);
